94512a7d90a92106b3f6d8b45687fac127289f0c
Longlasting1805 <longlasting1805@gmail.com>
2026-06-26 23:43:25 +0100

    Fix MongoDB connection

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
 const leadRoutes = require("./routes/leadRoutes");
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +16,5 @@ app.use(cors());
 app.use("/", leadRoutes);
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("🚀 Server running on port 3000");
 });
